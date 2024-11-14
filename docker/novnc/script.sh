007aae7a2ae18f21b5c5c4c10f944e2c9a0e6170
Arthur Fournier <arthur.g.fournier@gmail.com>
2024-11-14 21:12:11 +0100

    change config and add udev autostart

--- docker/novnc/script.sh
+++ docker/novnc/script.sh
@@ -72,6 +72,58 @@ fi
 echo -e "\e[34mDONE\e[0m"
 
 
+
+print_header "Configure udevd"
+
+# Since this container may also be run with CAP_SYS_ADMIN, ensure we can actually execute "udevadm trigger"
+run_dumb_udev="false"
+if [ ! -w /sys ]; then
+    # Disable supervisord script since we are not able to write to sysfs
+    print_step_header "Disable udevd - /sys is mounted RO"
+    sed -i 's|^autostart.*=.*$|autostart=false|' /app/conf.d/udex.conf
+    run_dumb_udev="true"
+elif [ ! -d /run/udev ]; then
+    # Disable supervisord script since we are not able to write to udev/data path
+    print_step_header "Disable udevd - /run/udev does not exist"
+    sed -i 's|^autostart.*=.*$|autostart=false|' /app/conf.d/udex.conf
+    run_dumb_udev="true"
+elif [ ! -w /run/udev ]; then
+    # Disable supervisord script since we are not able to write to udev/data path
+    print_step_header "Disable udevd - /run/udev is mounted RO"
+    sed -i 's|^autostart.*=.*$|autostart=false|' /app/conf.d/udex.conf
+    run_dumb_udev="false"
+elif udevadm trigger &> /dev/null; then
+    print_step_header "Configure container to run udev management"
+    # Enable supervisord script
+    sed -i 's|^autostart.*=.*$|autostart=true|' /app/conf.d/udex.conf
+    # Configure udev permissions
+    if [[ -f /lib/udev/rules.d/60-steam-input.rules ]]; then
+        sed -i 's/MODE="0660"/MODE="0666"/' /lib/udev/rules.d/60-steam-input.rules
+    fi
+    run_dumb_udev="false"
+else
+    # Disable supervisord script since we are not able to execute "udevadm trigger"
+    print_step_header "Disable udev service due to privilege restrictions"
+    sed -i 's|^autostart.*=.*$|autostart=false|' /app/conf.d/udex.conf
+    run_dumb_udev="true"
+fi
+
+if [ "${run_dumb_udev}" = "true" ]; then
+    # Enable dumb-udev instead of udevd
+    print_step_header "Enable dumb-udev service"
+    sed -i 's|^command.*=.*$|command=start-dumb-udev.sh|' /app/conf.d/udex.conf
+    sed -i 's|^autostart.*=.*$|autostart=true|' /app/conf.d/udex.conf
+fi
+
+
+if [[ -e /dev/uinput ]]; then
+    print_step_header "Ensure the default user has permission to r/w on input devices"
+    chmod 0666 /dev/uinput
+fi
+
+echo -e "\e[34mDONE\e[0m"
+
+
 #######################################################
 # Configure dbus
 print_header "Configure local"
